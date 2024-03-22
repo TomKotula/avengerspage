0f528de5f4952f75b371508aafeb328828eab1e5
Tom Kotula <test-git@youcode.pl>
2024-03-22 15:49:29 +0100

    gallery improved h3 changed into h2 in index.html and code formatted

--- script.js
+++ script.js
@@ -6,7 +6,7 @@
     const toggleBackground = () => {
         const body = document.querySelector(".body");
         const lightMode = document.querySelector(".lightMode");
-        
+
         body.classList.toggle("gray");
         lightMode.innerText = body.classList.contains("gray") ? "☀️" : "🌙";
     };
@@ -18,5 +18,38 @@
         welcome();
     };
 
+    function openModal() {
+        var modal = document.getElementById('lightboxModal');
+        modal.style.display = "block";
+
+        modal.onclick = function (event) {
+            if (event.target == modal) {
+                closeModal();
+            }
+        }
+    }
+
+    function closeModal() {
+        document.getElementById('lightboxModal').style.display = "none";
+    }
+
+    var slideIndex = 1;
+    showSlides(slideIndex);
+
+    function currentSlide(n) {
+        showSlides(slideIndex = n);
+    }
+
+    function showSlides(n) {
+        var i;
+        var slides = document.getElementsByClassName("modal__slides");
+        if (n > slides.length) { slideIndex = 1 }
+        if (n < 1) { slideIndex = slides.length }
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+        slides[slideIndex - 1].style.display = "block";
+    }
+    
     init();
 }
